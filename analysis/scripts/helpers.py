d359f55626450b8c6d15c76afb31014e574e7e6d
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2023-01-06 02:18:02 +0100

    graph split by day

--- analysis/scripts/helpers.py
+++ analysis/scripts/helpers.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import math
 import numpy as np
+from numpy import mean
+from numpy import var
+from math import sqrt
 
 
 def make_df_with_freq(df, freq):
@@ -41,22 +44,26 @@ def str2dt(date):
     """Convert a string to a pandas timestamp object """
     return pd.to_datetime(date, format= '%Y, %m, %d, %H, %M' )
 
-
-# # function to calculate Cohen's d for independent samples
-# def cohend(d1, d2):
-#     # calculate the size of samples
-#     n1, n2 = len(d1), len(d2)
-#     # calculate the variance of the samples
-#     s1, s2 = var(d1, ddof=1), var(d2, ddof=1)
-#     # calculate the pooled standard deviation
-#     s = sqrt(((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2))
-#     # calculate the means of the samples
-#     u1, u2 = mean(d1), mean(d2)
-#     # calculate the effect size
-#     return (u1 - u2) / s
-
+# function to calculate Cohen's d for independent samples
+def cohend(d1, d2):
+    # calculate the size of samples
+    n1, n2 = len(d1), len(d2)
+    # calculate the variance of the samples
+    s1, s2 = var(d1, ddof=1), var(d2, ddof=1)
+    # calculate the pooled standard deviation
+    s = sqrt(((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2))
+    # calculate the means of the samples
+    u1, u2 = mean(d1), mean(d2)
+    # calculate the effect size
+    return (u1 - u2) / s
 
 
-# def normalize(arr):
-#     arr2 =  (arr - arr.min())/(arr.max() - arr.min()) 
-#     return arr2
+def normalize(arr):
+    arr2 =  (arr - arr.min())/(arr.max() - arr.min()) 
+    return arr2
+
+
+def rmse(arr1, arr2):
+    MSE = np.square(np.subtract(arr1, arr2)).mean() 
+    RMSE = math.sqrt(MSE)
+    return RMSE
