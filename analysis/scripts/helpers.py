b05e6bfd2985f4aadf0d4d7a7ce94b3aaca30b6f
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2023-05-29 07:34:31 -0700

    look at different smoothing techniques

--- analysis/scripts/helpers.py
+++ analysis/scripts/helpers.py
@@ -6,21 +6,9 @@ from numpy import var
 from math import sqrt
 import json
 import plotly.graph_objects as go
+import scipy.optimize
 
 ## typical imports 
-# import plotly.graph_objects as go
-# from plotly.subplots import make_subplots
-# import pandas as pd
-# import numpy as np
-# import json
-
-# # local modules 
-# import sys
-# sys.path.insert(0, "../scripts")
-# from helpers import *
-# sys.path.insert(0, "../../JUST")
-# from JUSTjumps import *
-
 
 
 
@@ -267,3 +255,30 @@ def temperature_plot(df):
 
 
 ## ------------------------- ! End Plots 
+
+
+## ------------------------- ! Miscellaneous  
+
+def fit_sin(tt, yy):
+    '''Fit sin to the input time sequence, and return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"
+
+    # https://stackoverflow.com/questions/16716302/how-do-i-fit-a-sine-curve-to-my-data-with-pylab-and-numpy
+    
+    '''
+    tt = np.array(tt)
+    yy = np.array(yy)
+    ff = np.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
+    Fyy = abs(np.fft.fft(yy))
+    guess_freq = abs(ff[np.argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
+    guess_amp = np.std(yy) * 2.**0.5
+    guess_offset = np.mean(yy)
+    guess = np.array([guess_amp, 2.*np.pi*guess_freq, 0., guess_offset])
+
+    def sinfunc(t, A, w, p, c):  return A * np.sin(w*t + p) + c
+    popt, pcov = scipy.optimize.curve_fit(sinfunc, tt, yy, p0=guess)
+    A, w, p, c = popt
+    f = w/(2.*np.pi)
+    fitfunc = lambda t: A * np.sin(w*t + p) + c
+    return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": np.max(pcov), "rawres": (guess,popt,pcov)}
+
+## ------------------------- !End  Miscellaneous  
